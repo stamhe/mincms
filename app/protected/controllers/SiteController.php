662be62310706c1f3bf27b73526971d1cc14c156
mincms <68103403@qq.com>
2013-10-14 13:06:17 +0800

    when visit sitecontroller will remove cache

--- app/protected/controllers/SiteController.php
+++ app/protected/controllers/SiteController.php
@@ -9,7 +9,10 @@ class SiteController extends \application\core\FrontController
 	function init(){
 		parent::init();
 		$this->theme = 'default';
-		 
+		//安装完成后可删除
+		MinCache::delete('all_modules_alias');
+		MinCache::delete('all_modules');
+		MinCache::delete('hooks');
 	}
 	public function actions()
 	{
